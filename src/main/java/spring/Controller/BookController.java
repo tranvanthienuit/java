67cacf50bab9d02434b4420e4b489cf14f434363
Tran Van Thien <tranvanthienuit@gmail.com>
2022-06-14 15:55:31 +0700

    file

--- src/main/java/spring/Controller/BookController.java
+++ src/main/java/spring/Controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import spring.Entity.BookList;
@@ -23,6 +24,7 @@ import spring.Service.UserService;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
 public class BookController {
@@ -108,6 +110,10 @@ public class BookController {
     @PostMapping(value = {"/danh-gia-sach/{bookId}/{star}", "/danh-gia-sach"})
     public ResponseEntity<?> appriciateBook(@PathVariable(value = "bookId", required = false) String bookId, @PathVariable(value = "star", required = false) int star) {
         Rating rating = new Rating();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (Objects.equals(authentication.getName(), "anonymousUser")) {
+            return new ResponseEntity<>("error", HttpStatus.OK);
+        }
         userDetail userDetail = (userDetail) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User user = userService.findUserByUserId(userDetail.getUserId());
         Book book = booksService.findBookByBookId(bookId);
